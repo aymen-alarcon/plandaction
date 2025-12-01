3bdb643e597590f0e05aab803d6ae0e7fe41bfc0
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-12-01 15:35:11 +0100

    `Updated index.html and script.js to add submit button ID and modify local storage functionality`

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function CreateGamer() {
-    document.forms["submitNewGamer"]
-}
+let gamersList = []
 
+document.getElementById("submitBtn").addEventListener("click", (event)=>{
+    event.preventDefault()
+    let form = document.forms["submitNewGamer"]
+    
+    let gamer = {
+        firstName : form.FirstName.value,
+        LastName : form.LastName.value,
+        Game : form.Game.value,
+        Phone : form.Phone.value
+    }
+    
+    gamersList.push(gamer)
+    addItemsFromLocalStorage("Gamers", gamersList)
+})
 
 function getItemsFromLocalStorage(key) {
-    localStorage.getItem(key)
+    localStorage.getItem(JSON.parse(key))
 }
 
 function addItemsFromLocalStorage(key, list) {
-    localStorage.setItem(key, list)
+    localStorage.setItem(key, JSON.stringify(list))
 }
