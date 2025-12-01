34147f31a8d4834c5a6327c5862b6d30368c64a6
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-12-01 18:53:50 +0100

    `Added id property to gamer object, incremented count variable, and updated addItemsFromLocalStorage function to addItemsToLocalStorage. Added delete functionality for gamers.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+let count = 0;
+
 document.getElementById("submitBtn").addEventListener("click", (event)=>{
     event.preventDefault()
+    count++
     let form = document.forms["submitNewGamer"]
     
     let gamer = {
+        id : count,
         firstName : form.FirstName.value,
         LastName : form.LastName.value,
         Game : form.Game.value,
         Phone : form.Phone.value
     }
     
-    addItemsFromLocalStorage("gamers", gamer)
+    addItemsToLocalStorage("gamers", gamer)
     displayNewGamers()
     form.reset()
     form.FirstName.focus()
@@ -19,14 +23,16 @@ function getItemsFromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key)) || []
 }
 
-function addItemsFromLocalStorage(key, list) {
+function addItemsToLocalStorage(key, list) {
     let dataList = getItemsFromLocalStorage("gamers")
     dataList.push(list)
     localStorage.setItem(key, JSON.stringify(dataList))
 }
+
 document.addEventListener("DOMContentLoaded", ()=>{
     displayNewGamers()
 })
+
 function displayNewGamers() {
     let gamersList = getItemsFromLocalStorage("gamers")
     
@@ -37,7 +43,23 @@ function displayNewGamers() {
                 <div>${gamer.LastName}</div>
                 <div>${gamer.Game}</div>
                 <div>${gamer.Phone}</div>
+                <div class="btn btn-outline-danger" data-name="${gamer.id}">
+                    Delete
+                </div>
             </div>
         `
     });
+}
+
+document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-outline-danger")) {
+        deletePlayer(event.target.getAttribute("data-name"));
+    }
+});
+
+function deletePlayer(userId) {
+    let gamerId = userId - 1
+    let gamersList = getItemsFromLocalStorage("gamers")
+    gamersList.splice(gamerId, 1)
+    addItemsToLocalStorage("gamers", gamersList)
 }
